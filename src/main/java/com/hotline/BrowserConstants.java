70f233bb6981a932bcc8720c205f6f2d4965c220
Dmytro_Kapeliukh <welcome.t.t100@gmail.com>
2018-02-23 18:37:33 +0200

    browser version

--- src/main/java/com/hotline/BrowserConstants.java
+++ src/main/java/com/hotline/BrowserConstants.java
@@ -3,5 +3,5 @@ package com.hotline;
 public class BrowserConstants {
 
     public static String CHROME_PATH_MAC = "/Users/Dmytro_Kapeliukh/My projects/Hotline/resources/drivers/chromedriver/chromedriver";
-    public static String CHROME_PATH_WIN = "/Users/Dmytro_Kapeliukh/My projects/Hotline/resources/drivers/chromedriver/chromedriver";
+    public static String CHROME_PATH_WIN = "D:\\Hotline\\resources\\drivers\\chromedriver\\chromedriver.exe";
 }
